5e5681c88d60191151769e683769508fcd31db27
Мурзакаева_ИТ-17 <murzakaevaar2006@icloud.com>
2025-01-10 16:31:50 +0300

    Update and rename код to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,49 +1,8 @@
 #include <iostream>
 #include <string>
+#include "gcd_calculator.h"
+
 using namespace std;
-
-// Функция, проверяющая, является ли строка числом
-bool isValidNumber(const string& str) {
-    if (str.empty()) return false; // Проверка на пустую строку
-    for (char c : str) {
-        if (!isdigit(c)) { // Если есть хотя бы один нецифровой символ
-            return false;
-        }
-    }
-    return true;
-}
-
-// Функция для заполнения массива длинных целых чисел из строки
-void fillArray(long long* array, const string& str, int parts) {
-    int index = 0;
-    // Цикл для извлечения подстрок из строки
-    for (int i = str.length(); i > 0; i -= 9) {
-        // Извлекаю подстроку длиной до 9 символов
-        string substring = str.substr(max(0, i - 9), min(9, i));
-        // Преобразую подстроку в long long и сохраняю в массив
-        array[index++] = stoll(substring);
-    }
-}
-
-// Функция для нахождения НОД двух чисел
-long long gcd(long long a, long long b) {
-    // Алгоритм Евклида для нахождения НОД
-    while (b != 0) {
-        long long temp = b;
-        b = a % b;
-        a = temp;
-    }
-    return a; // Возвращаем НОД
-}
-
-// Функция для нахождения НОД трёх чисел
-long long gcdOfThree(long long* a, long long* b, long long* c, int parts) {
-    // Нахождение НОД между первым и вторым числами
-    long long result = gcd(a[0], b[0]);
-    // Нахождение НОД результата с третьим числом
-    result = gcd(result, c[0]);
-    return result; // Возвращаю НОД
-}
 
 int main() {
     setlocale(LC_ALL, "RU");
@@ -51,6 +10,7 @@ int main() {
     string s1, s2, s3;
     cout << "Нахождение наибольшего общего делителя трёх чисел (m, n, p >= MaxInt)" << endl;
 
+    // Ввод первого числа
     cout << "Введите первое число m: ";
     cin >> s1;
     while (!isValidNumber(s1)) {
@@ -58,6 +18,7 @@ int main() {
         cin >> s1;
     }
 
+    // Ввод второго числа
     cout << "Введите второе число n: ";
     cin >> s2;
     while (!isValidNumber(s2)) {
@@ -65,6 +26,7 @@ int main() {
         cin >> s2;
     }
 
+    // Ввод третьего числа
     cout << "Введите третье число p: ";
     cin >> s3;
     while (!isValidNumber(s3)) {
@@ -78,6 +40,7 @@ int main() {
     if (s3.length() > maxLength) maxLength = s3.length();
 
     // Вычисляем количество частей
+
     int parts = (maxLength + 8) / 9;
 
     // Создаём динамические массивы для хранения чисел
@@ -97,7 +60,6 @@ int main() {
     cout << "Наибольший общий делитель: " << result << endl;
 
     // Освобождение выделенной памяти
-
     delete[] a;
     delete[] b;
     delete[] c;
